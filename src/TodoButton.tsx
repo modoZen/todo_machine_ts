89c55b3c0074a3f1333c70b2943d39834cdfb3a7
Max <mdiazn@uni.pe>
2023-06-14 15:18:04 -0500

    05-eventos

--- src/TodoButton.tsx
+++ src/TodoButton.tsx
@@ -4,7 +4,16 @@ import './TodoButton.css'
 const TodoButton:FC = () => {
 
     return (
-        <button className="TodoButton">+</button>
+        <button 
+            className="TodoButton"
+            onClick={(event)=>{
+                console.log('le diste clic');
+                console.log(event);
+                console.log(event.target);
+            }}
+        >
+            +
+        </button>
     )
 }
 
--- src/TodoSearch.tsx
+++ src/TodoSearch.tsx
@@ -4,7 +4,17 @@ import './TodoSearch.css'
 const TodoSearch:FC = () => {
 
     return (
-       <input type="text" className="TodoSearch" placeholder="Ingresar nombre de tarea buscada" />
+       <input 
+        type="text" 
+        className="TodoSearch" 
+        placeholder="Ingresar nombre de tarea buscada" 
+        onChange={(event)=>{
+            console.log('Escribiste en el search');
+            console.log(event);
+            console.log(event.target);
+            console.log(event.target.value);
+        }}
+    />
     )
 }
 
